40feea92cd25b66a87bca0c387d8fa958d3bae42
limpido <43034953+limpido@users.noreply.github.com>
2025-12-09 00:03:06 +0800

    update

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -24,7 +24,11 @@ return {
           },
           sections = {
             lualine_a = {'mode'},
-            lualine_b = {'branch', 'diff', 'diagnostics'},
+            lualine_b = {
+		{'branch', icon = ""},
+		'diff',
+		'diagnostics'
+	    },
             lualine_c = {'filename'},
             lualine_x = {'encoding', 'fileformat', 'filetype'},
             lualine_y = {'progress'},
